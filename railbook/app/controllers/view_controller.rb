0e438607609b256aa1c022e99ef9a0552e965edc
寺越彬紘 <terakoshi.akihiro@adways.net>
2015-06-24 23:03:53 +0900

    chapter4_1 last

--- railbook/app/controllers/view_controller.rb
+++ railbook/app/controllers/view_controller.rb
@@ -1,6 +1,58 @@
-def form_tag
-  @book = Book.new
-end
-def form_for
-  @book = Book.new
+class ViewController < ApplicationController
+  def form_tag
+    @book = Book.new
+  end
+
+  def form_for
+    @book = Book.new
+  end
+
+  def field
+    @book = Book.new
+  end
+
+  def html5
+    @book = Book.new
+  end
+
+  def select
+    @book = Book.new(publish: '技術評論社')
+  end
+
+  def select_tag
+  end
+
+  def select_tag2
+  end
+
+  def col_select
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def col_select2
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def group_select
+    @review = Review.new
+    @authors = Author.all
+  end
+
+  def group_select2
+  end
+
+  def dat_select
+    @book = Book.find(980190963)
+  end
+
+  def col_radio
+    @book = Book.new(publish: '技術評論社')
+    @books = Book.select(:publish).distinct
+  end
+
+  def fields
+    @user = User.find(980190962)
+  end
 end
